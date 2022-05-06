bd114b51f92e1c2a016d453a2b2a4860431ef298
Ashok Thirunavukarasu <ashok.thirunavukarasu@impigertech.com>
2022-05-06 18:18:35 +0530

    Third Commit

--- Inheritence.py
+++ Inheritence.py
@@ -1,14 +1,17 @@
 #Parentclass
+from multiobjects import Ashok
+
+
 class Parent:
     def __init__(self, first_name, last_name):
         self.first_name = first_name
         self.last_name = last_name
     def method(self):
-        print(self.first_name,self.last_name)
+       print(self.first_name,self.last_name)
 obj = Parent("Ashok", "Thirunavukarasu")
 obj.method()
 #Childclass
 class child(Parent):
-    pass
+   pass
 obj1 = child("Ashok", "T")
 obj1.method()
