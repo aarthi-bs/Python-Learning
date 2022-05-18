7fd27e5d1e02939b44a2ff5595b5c64244c17505
Ashok <ashokthirunavkarasu@gmail.com>
2022-05-18 12:57:02 +0530

    Temp

--- createclass.py
+++ createclass.py
@@ -1,5 +1,8 @@
 class Ashok:
     def function(self):
         print("Python is fun")
+        print("C-Programming")
+    def function1(self):
+        print("Java")
 newobj = Ashok()
 print(newobj.function())
